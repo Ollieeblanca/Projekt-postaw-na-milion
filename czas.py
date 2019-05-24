08af181a7bb13b6d861977a31398030c9b513ff7
Ollieeblanca <50325445+Ollieeblanca@users.noreply.github.com>
2019-05-24 19:43:49 +0200

    Czas na odpowiedź

---
+++ czas.py
@@ -0,0 +1,18 @@
+import time
+
+print("Czas start!")
+for k in range(1,61):
+    print(k)
+    time.sleep(1)
+    if k == 30:
+        print("Minęła połowa czasu")
+
+###
+#do koła ratunkowego:
+###
+print("Czas start!")
+for k in range(1,21):
+    print(k)
+    time.sleep(1)
+    if k == 10:
+        print("Minęła połowa czasu")
